3e67a2a854e75bb97165fd5b791d7da829cf5761
Chris Brooks <chris@chrisbrooks.org>
2023-05-22 07:54:06 -0400

    Minor update to add comment

--- roman_test.py
+++ roman_test.py
@@ -1,5 +1,7 @@
 from roman import roman
 
 
+# Simple first test case - it should pass with
+# even the most trivial implementation.
 def test_one():
-	assert roman(1) == 'I'
+    assert roman(1) == "I"
